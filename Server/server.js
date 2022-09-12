8161a16582be841a849bb7702b40bf7411f045c7
alaa200112 <113264919+alaa200112@users.noreply.github.com>
2022-09-12 15:24:50 +0200

    getbookByid

--- Server/server.js
+++ Server/server.js
@@ -1,9 +1,9 @@
 var express = require('express')
 var app = express()
 var mongoose = require('mongoose')
-var bodyParser = require('body-parser')
 
 var book = require('./models/BookModel')
+const { response } = require('express')
 
 mongoose.connect('mongodb+srv://MuhammadAhmad_:v0qe18fEhI3Ilre9@e-commercedb.qlmze8a.mongodb.net/?retryWrites=true&w=majority')
   .then(() => {
@@ -23,5 +23,15 @@ app.get('/books', (req, resp) => {
     }).catch(err => {
         console.log('Failed to fetch data')
     })
+})
+
+app.get('/books/:id',(requst,response)=>{
+ let id=requst.params.id;
+   book.findById(id).then((singleBook)=>{
+       response.send(singleBook);
+   }).catch(()=>{
+    console.log('Not found id for book');
+   })
+
 })
 
